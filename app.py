04ef2f5676ece94b417005f5570c688ed43c7ade
William Ball <wball1@swarthmore.edu>
2019-09-28 10:57:09 -0400

    form working

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
 import csv
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from config import Config
+
+class LoginForm(FlaskForm):
+    zipcode = StringField('Zip Code', validators=[DataRequired()])
 
 app = Flask(__name__)
+app.config.from_object(Config)
 application = app
+
+@app.route('/', methods=['GET', 'POST'])
+def getzip():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/info')
+    return render_template('index.html', title='Hi', form=form)
 
 @app.route('/<item>')
 def put(item):
     return render_template('index.html', title='Hi', user={'username':item})
 
-@app.route("/")
+@app.route("/info")
 def index():
-    user = {'username': 'World'}
-    return render_template('index.html', title='Hi', user=user)
+    test_legal = [
+            {
+                'state': 'CA',
+                'service': 'representation',
+                'name': 'California Representation'
+            },
+            {
+                'state': 'PA',
+                'service': 'expungements',
+                'name': 'PLSE'
+            }
+    ]
+    return render_template('index.html', title='Hi', legal=test_legal)
 
 if __name__ == "__main__":
     app.run()
