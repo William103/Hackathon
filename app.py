aecfa879ca6e9ea7297fe7c3324dccf9a3dc5c9e
William Ball <wball1@swarthmore.edu>
2019-09-28 11:15:09 -0400

    removed file

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import csv
+import gmaps
 from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from config import Config
 
+# set up google maps api
+with open('apikey.txt') as f:
+    api_key = f.readline()
+gmaps.configure(api_key=api_key)
+new_york_coordinates = (40.75, -74.00)
+gmaps.figure(center=new_york_coordinates, zoom_level=12)
+
+
+# set up form
 class LoginForm(FlaskForm):
     zipcode = StringField('Zip Code', validators=[DataRequired()])
 
@@ -12,6 +22,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 application = app
 
+
+# get zip code
 @app.route('/', methods=['GET', 'POST'])
 def getzip():
     form = LoginForm()
@@ -19,10 +31,8 @@ def getzip():
         return redirect('/info')
     return render_template('index.html', title='Hi', form=form)
 
-@app.route('/<item>')
-def put(item):
-    return render_template('index.html', title='Hi', user={'username':item})
 
+# provide information
 @app.route("/info")
 def index():
     test_legal = [
